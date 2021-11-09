3f574bc6795ab15fb3d5f7ac30cea6080f215c55
Caroline Nielsen <caro112f@stud.kea.dk>
2021-11-09 10:20:25 +0100

    ændringer

--- wp-content/themes/ungdomsbyen-theme/page-kurser.php
+++ wp-content/themes/ungdomsbyen-theme/page-kurser.php
@@ -101,14 +101,14 @@ get_header();
 
 		function opretknapper () {
 			niveauer.forEach(niveau =>{
-				console.log(niveau knap)
+				console.log(niveauknap)
 				//lav en funktion der opretter knapper med kategori id som data attribut
 				document.querySelector("#filtrering").innerHTML += `<button class="filter" data-kursus="${niveau.name}">${niveau.name}</button>`
 				
 				
 			})
 			temaer.forEach(tema =>{
-				console.log(tema knap)
+				console.log(temaknap)
 				//lav en funktion der opretter knapper med kategori id som data attribut
 				document.querySelector("#tema-filtrering").innerHTML += `<button class="filter" data-tema="${tema.name}">${tema.name}</button>`
 				
